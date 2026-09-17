76d36e0f13645b289646ebbcbb299a0255db95ff
Soufiane Asrih <soufasri@emeal.nttdata.com>
2026-09-17 13:01:14 +0100

    fix

--- libs/ui/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/grid-tarjetas/grid-tarjetas.component';
-export * from './lib/vehicle-price-summary/vehicle-price-summary.component';
-export * from './lib/datos-persona/datos-persona.component';
